888438bcb1a0c69ecc797e91284a31c762913c99
KULAO1 <118022028+KULAO1@users.noreply.github.com>
2022-11-27 18:43:51 +0300

    Add files via upload

---
+++ Лабораторная работа 3_исправленное/task6.py
@@ -0,0 +1,14 @@
+list_numbers = [2, 90, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+index = 0
+max_number = list_numbers[0]
+
+for i, number in enumerate(list_numbers):
+    if number >= max_number:
+        max_number = number
+        index = i
+
+list_numbers[index], list_numbers[-1] = list_numbers[-1], list_numbers[index]
+
+
+print(list_numbers)  # Ответ [2, 90, -2, 8, -36, -44, -1, -85, -14, 25, -22, -90, -100, -8, 38, -92, -45, 67, 53, 90]
