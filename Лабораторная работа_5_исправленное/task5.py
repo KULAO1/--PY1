1b40e08302fc332a236827893f8f4bef77f58326
KULAO1 <118022028+KULAO1@users.noreply.github.com>
2022-12-11 12:48:54 +0300

    Update task5.py
    Здравствуйте! Сделала импорт библиотек сначала.

--- Лабораторная работа_5_исправленное/task5.py
+++ Лабораторная работа_5_исправленное/task5.py
@@ -1,10 +1,10 @@
 from string import ascii_uppercase, ascii_lowercase, digits
+from random import sample
 
 
 def get_random_password(n) -> str:
 
     str_ = ascii_uppercase + ascii_lowercase + digits
-    from random import sample
     str_ = "".join(sample(str_, n))
 
     return str_
